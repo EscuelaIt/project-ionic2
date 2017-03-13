c3121f0d730aaf09864785fe41bea8f051f2ba91
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2017-03-13 14:02:51 -0500

    add login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,22 +1,62 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, LoadingController } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AngularFireAuth } from 'angularfire2';
 
-/*
-  Generated class for the Login page.
+import { TabsPage } from '../tabs/tabs';
+import { RegisterPage } from '../register/register';
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  loginForm: FormGroup;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public loadCtrl: LoadingController,
+    public fireAuth: AngularFireAuth,
+    public fb: FormBuilder,
+  ) {
+    this.loginForm = this.makeLoginForm();
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+  }
+
+  doLogin( event: Event ){
+    event.preventDefault();
+    let load = this.loadCtrl.create();
+    load.present( load );
+    let email = this.loginForm.value.email;
+    let password = this.loginForm.value.password;
+    this.fireAuth.login({
+      email: email,
+      password: password
+    })
+    .then(user =>{
+      load.dismiss();
+      this.navCtrl.setRoot( TabsPage );
+    })
+    .catch(error =>{
+      console.log( error );
+      load.dismiss();
+    })
+  }
+
+  goToRegisterPage(){
+    this.navCtrl.push( RegisterPage );
+  }
+
+  private makeLoginForm(){
+    return this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
   }
 
 }

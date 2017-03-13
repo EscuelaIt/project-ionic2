33f9196f2bf10ec451c5da265bd5d63cba56ba42
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2017-03-13 15:11:30 -0500

    add searchbar

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -13,10 +13,67 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class SearchPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  query: string = '';
+
+  photosCopy: any = [];
+  photos:any[] = [
+    {
+      img: 'assets/img/img1.jpg',
+      text: 'cochabamba',
+    },
+    {
+      img: 'assets/img/img2.jpg',
+      text: 'bogota',
+    },
+    {
+      img: 'assets/img/img3.jpg',
+      text: 'bogota',
+    },
+    {
+      img: 'assets/img/img4.jpg',
+      text: 'cali',
+    },
+    {
+      img: 'assets/img/img5.jpg',
+      text: 'madrid',
+    },
+    {
+      img: 'assets/img/img6.jpg',
+      text: 'buenos aires',
+    },
+    {
+      img: 'assets/img/img7.jpg',
+      text: 'santiago',
+    },
+    {
+      img: 'assets/img/img8.jpg',
+      text: 'lima',
+    },
+    {
+      img: 'assets/img/img9.jpg',
+      text: 'lima',
+    }
+  ];
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams
+  ) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad SearchPage');
+    this.photosCopy = this.photos;
+  }
+
+  onInput(event: Event){
+    this.photos = this.photosCopy;
+
+    let val = this.query;
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.photos = this.photos.filter((photo) => {
+        return (photo.text.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
   }
 
 }
